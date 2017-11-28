deb7c6fcf557a6924e5e133b4d583ac05e32826e
Pierluigi Bortignon <bortigno@gmail.com>
2017-11-28 16:38:23 +0100

    some optimisation

--- genproduction/lhe_gen-sim_steps.sh
+++ genproduction/lhe_gen-sim_steps.sh
@@ -10,21 +10,33 @@ fi
 cd CMSSW_10_0_0_pre1/src
 eval `scram runtime -sh`
 
-JOB_LABEL=zd2j_m35
 INPUT_FRAGMENT=externalLHEProducer_and_PYTHIA8_Hadronizer_cff.py
 GRIDPACK_LOCATION=/afs/cern.ch/work/b/bortigno/darkphotons/genproductions/bin/MadGraph5_aMCatNLO/
-GRIDPACK_NAME=darkphoton_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz.v9.good.upto2j.MZd35.eps2e-2.auto-width.tar.xz
-OUTPUT_FRAGMENT=${INPUT_FRAGMENT}_${JOB_LABEL}
 
-sed -e s#GRIDPACKNAME#${GRIDPACK_NAME}#g  ${INPUT_FRAGMENT} > ${OUTPUT_FRAGMENT}
+ZDMASS=35
+NOFJET=2
+ESPILON=2e-2
+
+#GRIDPACK_NAME=darkphoton_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz.v9.good.upto2j.MZd35.eps2e-2.auto-width.tar.xz
+GRIDPACK_NAME=darkphoton_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz.ZD_UpTo2j_MZD${ZDMASS}_Eps${ESPILON}.tar.xz
+
+JOB_LABEL=zd${NOFJET}j_mzd${ZDMASS}
+OUTPUT_FRAGMENT=${INPUT_FRAGMENT/_cff.py/}_${JOB_LABEL}_cff.py
+
+GLOBALTAG=93X_mc2017_realistic_v3
+NEVENTS=999
+
+cd ../../
+sed -e s#GRIDPACKNAME#${GRIDPACK_NAME}#g  ${INPUT_FRAGMENT} > CMSSW_10_0_0_pre1/src/${OUTPUT_FRAGMENT}
+cd -
 
 #[ -s ${GRIDPACK_LOCATION}${GRIDPACK_NAME} ] || cp ${GRIDPACK_LOCATION}${GRIDPACK_NAME} . # not needed as the gridpack can be sent to the sandbox directly from the crab_cfg
 
 [ -d Configuration/GenProduction/python/ThirteenTeV/Hadronizer ] || mkdir -p Configuration/GenProduction/python/ThirteenTeV/Hadronizer
-cp ${OUTPUT_FRAGMENT} Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT}_cff.py
-[ -s Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT}_cff.py ] || exit $?;
+cp ${OUTPUT_FRAGMENT} Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT}
+[ -s Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT} ] || exit $?;
 
 scram b
 cd ../../
-cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT} --fileout file:${JOB_LABEL}_GEN-SIM-LHE.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 93X_mc2017_realistic_v3 --beamspot Realistic25ns13TeVEarly2017Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename ${JOB_LABEL}_GEN-SIM-LHEcfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 4941 || exit $? ; 
-
+echo cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT} --fileout file:${JOB_LABEL}_LHE-GEN-SIM.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions ${GLOBALTAG}  --beamspot Realistic25ns13TeVEarly2017Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename ${OUTPUT_FRAGMENT/_cff/_LHE-GEN-SIM_cfg} --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${NEVENTS} || exit $? ; 
+cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/Hadronizer/${OUTPUT_FRAGMENT} --fileout file:${JOB_LABEL}_LHE-GEN-SIM.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions ${GLOBALTAG}  --beamspot Realistic25ns13TeVEarly2017Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename ${OUTPUT_FRAGMENT/_cff/_LHE-GEN-SIM_cfg} --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${NEVENTS} || exit $? ; 
