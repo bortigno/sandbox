ddb91f77c44eac273e3b1fe818a9c89d2e620a81
Pierluigi Bortignon <bortigno@gmail.com>
2017-12-04 16:14:38 +0100

    First succesful version for LHE-GEN-SIM crab production. Had to disable the monitoring. The .lhe are still not tranferred because they are produced in the first step in the lheevent/cmsgrid_final.lhe but the script removes it on the way so at the end of cmsRun job the folder, nor the file are still accessible. Need an intermediate data handling.

--- genproduction/crab_cfg.py
+++ genproduction/crab_cfg.py
@@ -1,6 +1,10 @@
 from CRABClient.UserUtilities import config
 config = config()
 
+import datetime, time
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M')
+
 zd_mass = '35'
 number_of_jets = '2'
 epsilon = '2e-2'
@@ -8,19 +12,21 @@ custom_string_for_dataset = ''
 step = 'LHE-GEN-SIM'
 
 job_label = 'ZD_UpTo'+number_of_jets+'j_MZD'+zd_mass+'_Eps'+epsilon
-config.General.requestName = 'mc_genproduction_'+step+'_'+job_label+custom_string_for_dataset
+config.General.requestName = st+'mc_genproduction_'+step+'_'+job_label+custom_string_for_dataset
+config.General.transferOutputs = True
+config.General.transferLogs = False
 
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = 'externalLHEProducer_and_PYTHIA8_Hadronizer_zd'+number_of_jets+'j_mzd'+zd_mass+'_'+step+'_cfg.py'
-config.JobType.inputFiles = ['/afs/cern.ch/user/b/bortigno/public/darkphoton_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz.'+job_label+'.tar.xz']
+config.JobType.inputFiles = ['externalLHEProducer_and_PYTHIA8_Hadronizer_cff.py','/afs/cern.ch/work/b/bortigno/darkphotons/genproductions/bin/MadGraph5_aMCatNLO/darkphoton_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz.'+job_label+'.tar.xz']
 config.JobType.numCores = 8
-config.JobType.scriptExe = 'lhe_gen_sim_steps.sh'
-config.JobType.outputFiles = ['cmsgrid_final.lhe']
+config.JobType.scriptExe = 'lhe_gen-sim_steps.sh'
+#config.JobType.outputFiles = ['cmsgrid_final.lhe']
 
 config.Data.outputPrimaryDataset = job_label
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 1000 # the number of events here must match the number of events in the exeternalLHEProducer
-NJOBS = 100
+NJOBS = 10
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/bortigno/mc_genproduction/darkphoton/'+step+'/'+job_label
 config.Data.outputDatasetTag = 'PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-'+step
@@ -28,4 +34,5 @@ config.Data.publication = True
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 
 #config.Site.whitelist = ['T2_CH_CERN']
+config.Site.blacklist = ['T2_PK_NCP','T2_KR_KNU']
 config.Site.storageSite = 'T2_CH_CERN'
--- genproduction/externalLHEProducer_and_PYTHIA8_Hadronizer_cff.py
+++ genproduction/externalLHEProducer_and_PYTHIA8_Hadronizer_cff.py
@@ -4,7 +4,20 @@ import os
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
+# Avoid to have multiple lumi in the same job and therefore auto-cleaning
+# This means that it need an argument to be run
+# from FWCore.ParameterSet.VarParsing import VarParsing
+# options = VarParsing ('analysis')
+# options.register('jobNum', 0, VarParsing.multiplicity.singleton,VarParsing.varType.int,"jobNum")
+# options.parseArguments ()
+# firstLumi=10*options.jobNum+1 ## eventsPerJob/eventsPerLumi*jobNum +1
+# source = cms.Source("EmptySource",
+#         firstLuminosityBlock  = cms.untracked.uint32(firstLumi),
+#         numberEventsInLuminosityBlock = cms.untracked.uint32(100)
+#         )
 
+
+# External lHE producer configuration
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
     args = cms.vstring( 'THISDIR' + '/GRIDPACKNAME'),
     nEvents = cms.untracked.uint32(5000),
@@ -13,7 +26,7 @@ externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
     scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
 )
 
-
+# Hadronizer configuration
 generator = cms.EDFilter("Pythia8HadronizerFilter",
     maxEventsToPrint = cms.untracked.int32(1),
     pythiaPylistVerbosity = cms.untracked.int32(1),
