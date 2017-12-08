fcf5da5390517eb6055a579a9f9de8040f6aa732
Pierluigi Bortignon <bortigno@gmail.com>
2017-12-08 17:03:32 +0100

    adapting to the new submitting script. giving a thrid argument as the place from where the config is run and therefore where the output is stored.

--- genproduction/submit_qcut_hadronizer_batch_job.sh
+++ genproduction/submit_qcut_hadronizer_batch_job.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 # Prepare and launch the hadronizer job in lxbatch 
 
-mkdir hadronizer
+WORKINGDIR=/afs/cern.ch/work/b/bortigno/sandbox/genproduction/CMSSW_10_0_0_pre1/src/
+STORAGEDIR=/eos/cms/store/user/bortigno/MadGraph/darkphoton/mzd35/
+
+[ -d hadronizer ] || mkdir hadronizer
 
 for i in `seq 10 20` ; do sed -e s#QCUTREPLACE#$i#g PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM_cfg.py > hadronizer/PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM-QCUT_${i}_cfg.py ; done
 
-for i in `seq 10 20` ; do bsub -q 1nd -J qcut${i} -o hadronizer_${4}.out -e hadronizer_{i}.err ~/bin/bjob_luncher.sh /afs/cern.ch/work/b/bortigno/sandbox/genproduction/hadronizer/PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM-QCUT_${i}_cfg.py /afs/cern.ch/work/b/bortigno/sandbox/genproduction/CMSSW_10_0_0_pre1/src/ ; done
+for i in `seq 10 20` ; do bsub -q 1nd -J qcut${i} -o hadronizer_${4}.out -e hadronizer_${i}.err ~/bin/bjob_luncher.sh /afs/cern.ch/work/b/bortigno/sandbox/genproduction/hadronizer/PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM-QCUT_${i}_cfg.py ${WORKINGDIR} ${STORAGEDIR} ; done
 
 exit
