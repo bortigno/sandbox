26271fa6464a0adc0766722c8ffa50cd0827cf03
Pierluigi Bortignon <bortigno@gmail.com>
2017-12-11 11:09:48 +0100

    fix hadroniser.log output name

--- genproduction/submit_qcut_hadronizer_batch_job.sh
+++ genproduction/submit_qcut_hadronizer_batch_job.sh
@@ -8,6 +8,6 @@ STORAGEDIR=/eos/cms/store/user/bortigno/MadGraph/darkphoton/mzd35/
 
 for i in `seq 10 20` ; do sed -e s#QCUTREPLACE#$i#g PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM_cfg.py > hadronizer/PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM-QCUT_${i}_cfg.py ; done
 
-for i in `seq 10 20` ; do bsub -q 1nd -J qcut${i} -o hadronizer_${4}.out -e hadronizer_${i}.err ~/bin/bjob_luncher.sh /afs/cern.ch/work/b/bortigno/sandbox/genproduction/hadronizer/PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM-QCUT_${i}_cfg.py ${WORKINGDIR} ${STORAGEDIR} ; done
+for i in `seq 10 20` ; do bsub -q 1nd -J qcut${i} -o hadronizer_${i}.out -e hadronizer_${i}.err ~/bin/bjob_luncher.sh /afs/cern.ch/work/b/bortigno/sandbox/genproduction/hadronizer/PYTHIA8_Hadronizer_zd2j_mzd35_LHE-GEN-SIM-QCUT_${i}_cfg.py ${WORKINGDIR} ${STORAGEDIR} ; done
 
 exit
