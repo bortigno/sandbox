f8b70258851e89739011e1cafff926724db6a1bd
Pierluigi Bortignon <bortigno@gmail.com>
2018-02-21 15:35:36 +0100

    extending to all in one script

--- genproduction/gen-sim-raw_aodsim_steps.sh
+++ genproduction/gen-sim-raw_aodsim_steps.sh
@@ -9,11 +9,31 @@ fi
 cd CMSSW_10_0_0_pre1/src
 eval `scram runtime -sh`
 
+samplename="darkphoton"
+nEvents=10
 
 scram b
 cd ../../
-cmsDriver.py step1 --filein "dbs:/ZD_UpTo2j_MZD125_Eps2e-2/bortigno-PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-e4a3eca9ea42f5248633ece70b42f936/USER instance=prod/phys03" --fileout file:DP_MZd125Epsilon2e-2_fall17_GEN-SIM-RAW_step2.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-MC_v2_94X_mc2017_realistic_v9-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v10 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --python_filename DP_MZd125Epsilon2e-2_RunIIFall17DRPremix_GEN-SIM-RAW_step2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 1751 || exit $? ; 
+
+
+echo "================= cmsDriver preparing Step 2 ====================" | tee -a job.log
+# Preparing the configuration for running GEN-SIM-RAW aka step2
+cmsDriver.py step2 --filein "dbs:/ZD_UpTo2j_MZD125_Eps2e-2/bortigno-PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-e4a3eca9ea42f5248633ece70b42f936/USER instance=prod/phys03" --fileout file:${samplename}_fall17_GEN-SIM-RAW_step2.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-MC_v2_94X_mc2017_realistic_v9-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v10 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --python_filename ${samplename}_RunIIFall17DRPremix_GEN-SIM-RAW_step2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${nEvents} || exit $? ; 
 #file:/afs/cern.ch/user/b/bortigno/workspace/darkphotons/dp_mc_genproduction_fall17/DP_MZd35Epsilon2e-2_fall17.root"
 
-cmsDriver.py step2 --filein file:DP_MZd125Epsilon2e-2_fall17_GEN-SIM-RAW_step2.root --fileout file:DP_MZd125Epsilon2e-2_fall17_AODSIM_step3.root --mc --eventcontent AODSIM runUnscheduled --datatier AODSIM --conditions 94X_mc2017_realistic_v10 --step RAW2DIGI,RECO,RECOSIM,EI --nThreads 8 --era Run2_2017 --python_filename DP_MZd125Epsilon2e-2_RunIIFall17DRPremix_AODSIM_step3_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 1751 || exit $? ; 
+echo "================= CMSRUN starting step 2 ====================" | tee -a job.log
+# and now running GEM-SIM-RAW
+cmsRun -e -j ${samplename}_step2.log ${samplename}_RunIIFall17DRPremix_GEN-SIM-RAW_step2_cfg.py
 
+
+echo "================= cmsDriver preparing step 3 ====================" | tee -a job.log
+# Preparing configuration for running RAW-RECO-AODSIM aka step3
+cmsDriver.py step3 --filein file:${samplename}_fall17_GEN-SIM-RAW_step2.root --fileout file:${samplename}_fall17_AODSIM_step3.root --mc --eventcontent AODSIM runUnscheduled --datatier AODSIM --conditions 94X_mc2017_realistic_v10 --step RAW2DIGI,RECO,RECOSIM,EI --nThreads 8 --era Run2_2017 --python_filename ${samplename}_RunIIFall17DRPremix_AODSIM_step3_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${nEvents} || exit $? ; 
+
+echo "================= CMSRUN starting step 3 ====================" | tee -a job.log
+# and now running it
+cmsRun -e -j FrameworkJobReport.xml ${samplename}_RunIIFall17DRPremix_AODSIM_step3_cfg.py
+
+echo "================= Cleaning up step 2 output ====================" | tee -a job.log
+# cleaning
+rm -r ${samplename}_fall17_GEN-SIM-RAW_step2.root
