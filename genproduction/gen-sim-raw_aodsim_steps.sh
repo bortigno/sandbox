a26d0622bafb6b6353887f0306c5b26b1dc2ff12
Pierluigi Bortignon <bortigno@gmail.com>
2018-03-09 18:08:57 +0100

    some minor progress

--- genproduction/gen-sim-raw_aodsim_steps.sh
+++ genproduction/gen-sim-raw_aodsim_steps.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc630
-if [ -r CMSSW_10_0_o_pre1/src ] ; then 
- echo release CMSSW_10_0_0_pre1 already exists
+CMSSW_release=CMSSW_9_4_0_patch1
+
+if [ -r ${CMSSW_release}/src ] ; then 
+ echo release ${CMSSW_release} already exists
 else
-scram p CMSSW CMSSW_10_0_0_pre1
+scram p CMSSW ${CMSSW_release}
 fi
-cd CMSSW_10_0_0_pre1/src
+cd ${CMSSW_release}/src
 eval `scram runtime -sh`
 
 samplename="darkphoton"
 nEvents=10
+
+[ -d Configuration/GenProduction/python/ ] || mkdir -p Configuration/GenProduction/python/
+[ -s Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py ] || cp ../../Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py Configuration/GenProduction/python/
 
 scram b
 cd ../../
@@ -18,7 +23,8 @@ cd ../../
 
 echo "================= cmsDriver preparing Step 2 ====================" | tee -a job.log
 # Preparing the configuration for running GEN-SIM-RAW aka step2
-cmsDriver.py step2 --filein "dbs:/ZD_UpTo2j_MZD125_Eps2e-2/bortigno-PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-e4a3eca9ea42f5248633ece70b42f936/USER instance=prod/phys03" --fileout file:${samplename}_fall17_GEN-SIM-RAW_step2.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-MC_v2_94X_mc2017_realistic_v9-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v10 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --python_filename ${samplename}_RunIIFall17DRPremix_GEN-SIM-RAW_step2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${nEvents} || exit $? ; 
+#cmsDriver.py Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py --filein "dbs:/ZD_UpTo2j_MZD125_Eps2e-2/bortigno-PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-e4a3eca9ea42f5248633ece70b42f936/USER instance=prod/phys03" --fileout file:${samplename}_fall17_GEN-SIM-RAW_step2.root --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v10 --step GEN,SIM,DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --python_filename ${samplename}_RunIIFall17DRPremix_GEN-SIM-RAW_step2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${nEvents} || exit $? ;
+cmsDriver.py Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py --filein "dbs:/ZD_UpTo2j_MZD125_Eps2e-2/bortigno-PUMoriond17-Realistic25ns13TeVEarly2017Collision-93X_mc2017_realistic_v3-LHE-e4a3eca9ea42f5248633ece70b42f936/USER instance=prod/phys03" --fileout file:${samplename}_fall17_GEN-SIM-RAW_step2.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-MC_v2_94X_mc2017_realistic_v9-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v10 --step GEN,SIM,DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --python_filename ${samplename}_RunIIFall17DRPremix_GEN-SIM-RAW_step2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n ${nEvents} || exit $? ; 
 #file:/afs/cern.ch/user/b/bortigno/workspace/darkphotons/dp_mc_genproduction_fall17/DP_MZd35Epsilon2e-2_fall17.root"
 
 echo "================= CMSRUN starting step 2 ====================" | tee -a job.log
