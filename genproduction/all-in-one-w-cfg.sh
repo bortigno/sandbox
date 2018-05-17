99c826d924dbfec0837ef2f262e39a13918ff7f8
Pierluigi Bortignon <bortigno@gmail.com>
2018-05-17 22:21:39 +0200

    first version of all-in-one with cfg for step2 and step3 for testing

---
+++ genproduction/all-in-one-w-cfg.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+ConfigRelease=CMSSW_9_4_0_patch1
+RunningRelease=${ConfigRelease}
+
+SANDBOXDIR=${PWD}
+
+echo "================= PB: Starting cmssw environment prepration ====================" | tee -a job.log
+
+export SCRAM_ARCH=slc6_amd64_gcc630
+if [ -r ${ConfigRelease}/src ] ; then 
+ echo release ${ConfigRelease} already exists
+else
+scram p CMSSW ${ConfigRelease}
+fi
+cd ${ConfigRelease}/src
+eval `scram runtime -sh`
+
+INPUT_FRAGMENT=externalLHEProducer_cff.py
+
+#Print out the list of arguments passed to the script. The first argument is always the job number. Then begin the custom arguments.
+echo $@
+echo "param 1 = Zdmass = ${2}"
+echo "param 2 = nEvents = ${3}"
+ZDMASS="${2#*=}"
+NOFJET=2
+ESPILON=2e-2
+GLOBALTAG=93X_mc2017_realistic_v3
+NEVENTS="${3#*=}"
+samplename="darkphoton"
+
+echo "================= PB: Input Paramateres ========================================"  | tee -a job.log
+echo $ZDMASS
+echo $NOFJET
+echo $ESPILON
+echo $GLOBALTAG
+echo $NEVENTS
+echo $samplename
+
+#for crab GRIDPACK_LOCATION should be in the sandbox directoruy - and the gridpack should be copied in the sandbox.tar.gz using the crab config...
+GRIDPACK_LOCATION=${SANDBOXDIR}
+# for local testing the full path should be given
+#GRIDPACK_LOCATION=/afs/cern.ch/user/b/bortigno/workspace/sandbox/genproduction/
+
+GRIDPACK_NAME=darkphoton_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz.ZD_UpTo2j_MZD${ZDMASS}_Eps${ESPILON}.tar.xz
+
+echo "================= PB: Gridpack name ========================================"  | tee -a job.log
+echo $GRIDPACK_NAME
+
+echo "================= PB: Preparing the configs from gragments ====================" | tee -a job.log
+
+JOB_LABEL=zd${NOFJET}j_mzd${ZDMASS}
+OUTPUT_FRAGMENT=${INPUT_FRAGMENT/_cff.py/}_${JOB_LABEL}_cff.py
+OUTPUT_FRAGMENT_STEP2=step2_cfg.py
+OUTPUT_FRAGMENT_STEP3=step3_cfg.py
+
+
+cd ${SANDBOXDIR}
+sed -e s#GRIDPACKNAME#${GRIDPACK_NAME}#g  ${INPUT_FRAGMENT} > ${ConfigRelease}/src/${OUTPUT_FRAGMENT}
+sed -i s#THISDIR#${GRIDPACK_LOCATION}#g ${ConfigRelease}/src/${OUTPUT_FRAGMENT}
+cd -
+
+#This is for step1
+[ -d Configuration/GenProduction/python/ThirteenTeV/LHE ] || mkdir -p Configuration/GenProduction/python/ThirteenTeV/LHE
+cp -v ${OUTPUT_FRAGMENT} Configuration/GenProduction/python/ThirteenTeV/LHE/${OUTPUT_FRAGMENT}
+[ -s Configuration/GenProduction/python/ThirteenTeV/LHE/${OUTPUT_FRAGMENT} ] || exit $?;
+
+# This is for step2
+[ -d Configuration/GenProduction/python/ ] || mkdir -p Configuration/GenProduction/python/
+cp -v ../../Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py Configuration/GenProduction/python/
+
+# This is for the customisation for all steps
+cp -v ../../customiseGenSimRawAodsim.py Configuration/GenProduction/python/
+
+
+echo "================= PB: Compiling fragments from release ====================" | tee -a job.log
+
+scram b
+cd ../../
+CONFIG_TO_RUN=${OUTPUT_FRAGMENT/_cff/_LHE_cfg}
+
+echo "================= PB: Running cmsDriver ====================" | tee -a job.log
+# Preparing the configuration for running LHE aka step1
+echo cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/LHE/${OUTPUT_FRAGMENT} --fileout file:${samplename}_lhe.root --mc --eventcontent LHE --datatier LHE --conditions ${GLOBALTAG}  --beamspot Realistic25ns13TeVEarly2017Collision --step LHE --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename ${CONFIG_TO_RUN} --no_exec  -n ${NEVENTS} || exit $? ; 
+cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/LHE/${OUTPUT_FRAGMENT} --fileout file:${samplename}_lhe.root --mc --eventcontent LHE --datatier LHE --conditions ${GLOBALTAG}  --beamspot Realistic25ns13TeVEarly2017Collision --step LHE --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename ${CONFIG_TO_RUN} --no_exec -n ${NEVENTS} || exit $? ; 
+
+echo "================= PB: Dumping step1 config file ====================" | tee -a job.log
+cat ${CONFIG_TO_RUN}
+
+
+# ========= Now run the LHE =============
+echo "================= PB: Now start the setup for running the LHE ====================" | tee -a job.log
+
+
+echo "================= PB: CMSRUN starting jobNum=$1 ====================" | tee -a job.log
+echo "================= PB: CMSRUN setting up " ${RunningRelease} " ===================="| tee -a job.log
+if [ -r ${RunningRelease}/src ] ; then 
+     echo release ${RunningRelease} already exists
+ else
+     scram p CMSSW ${RunningRelease}
+ fi
+
+cd ${RunningRelease}/src
+eval `scram runtime -sh`
+
+scram b
+cd ../../
+
+echo "================= PB: CMSRUN starting Step 1 ====================" | tee -a job.log
+echo cmsRun -e -j ${samplename}_step1.log -p $CONFIG_TO_RUN jobNum=$1
+cmsRun -e -j ${samplename}_step1.log -p $CONFIG_TO_RUN 
+# jobNum=$1
+
+echo "================= PB: checking outputfile ====================" | tee -a job.log
+[ -s ${samplename}_lhe.root ] || echo "==================== ERROR: LHE output file not present ======================" 
+[ -s ${samplename}_lhe.root ] || exit $?
+
+echo "================= PB: CMSRUN starting Step 2 ====================" | tee -a job.log
+echo cmsRun -e -j ${samplename}_step2.log ${OUTPUT_FRAGMENT_STEP2}
+cmsRun -e -j ${samplename}_step2.log ${OUTPUT_FRAGMENT_STEP2}
+
+#cleaning
+rm -rfv ${samplename}_lhe.root
+
+echo "================= PB: checking outputfile ====================" | tee -a job.log
+[ -s ${samplename}_gen-sim-raw.root ] || echo "================= ERROR: GEN--SIM-RAW output file not present =====================" 
+[ -s ${samplename}_gen-sim-raw.root ] || exit $?
+
+
+echo "================= PB: CMSRUN starting step 3 ====================" | tee -a job.log
+echo cmsRun -e -j FrameworkJobReport.xml ${OUTPUT_FRAGMENT_STEP3}
+cmsRun -e -j FrameworkJobReport.xml ${OUTPUT_FRAGMENT_STEP3}
+
+echo "================= Cleaning up step 2 output ====================" | tee -a job.log
+# cleaning
+rm -rfv ${samplename}_gen-sim-raw.root
+
+
---
+++ genproduction/step2_cfg.py
@@ -0,0 +1,198 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py --filein file:darkphoton_lhe.root --fileout file:darkphoton_gen-sim-raw.root --pileup_input das:/Neutrino_E-10_gun/RunIISummer17PrePremix-MC_v2_94X_mc2017_realistic_v9-v1/GEN-SIM-DIGI-RAW site=T2_CH_CERN --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v11 --step GEN,SIM,DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --no_exec --customise=Configuration/GenProduction/customiseGenSimRawAodsim.noLumiCheck --customise=Configuration/DataProcessing/Utils.addMonitoring --customise=Configuration/GenProduction/customiseGenSimRawAodsim.randomSeed --python_filename=darkphoton_step2_cfg.py -n -1
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('HLT',eras.Run2_2017)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.DigiDMPreMix_cff')
+process.load('SimGeneral.MixingModule.digi_MixPreMix_cfi')
+process.load('Configuration.StandardSequences.DataMixerPreMix_cff')
+process.load('Configuration.StandardSequences.SimL1EmulatorDM_cff')
+process.load('Configuration.StandardSequences.DigiToRawDM_cff')
+process.load('HLTrigger.Configuration.HLT_2e34v40_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+    fileNames = cms.untracked.vstring('file:darkphoton_lhe.root'),
+    inputCommands = cms.untracked.vstring('keep *', 
+        'drop LHEXMLStringProduct_*_*_*'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_MLM_5f_max2j_LHE_pythia8_cff.py nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.PREMIXRAWoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:darkphoton_gen-sim-raw.root'),
+    outputCommands = process.PREMIXRAWEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+#from puFile import puListFull
+#import random
+#random.shuffle(puListFull)
+#puList= puListFull[0:20]
+#
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+process.mix.digitizers = cms.PSet(process.theDigitizersMixPreMix)
+#process.mixData.input.fileNames = cms.untracked.vstring(puList)
+
+process.mixData.input.fileNames = cms.untracked.vstring(['/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30042/64252196-EDD3-E711-BEB5-002590A88800.root', '/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30042/BC1EFA79-EED3-E711-8893-A4BF011253C0.root', '/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30000/BC2F3F07-ACD3-E711-BCC2-001E67792872.root', '/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30040/1E4A0C8E-EFD3-E711-AFBB-001E6739811A.root', '/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30042/782DFE65-EDD3-E711-AC1B-001E673969FF.root', '/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30041/D6FA398B-EDD3-E711-BD9D-001E67E71BC8.root', '/store/mc/RunIISummer17PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MC_v2_94X_mc2017_realistic_v9-v1/30041/96C95837-EED3-E711-B4DB-001E67E6F5EE.root'])
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v11', '')
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CP5Settings', 
+            'processParameters'),
+        processParameters = cms.vstring('JetMatching:setMad = off', 
+            'JetMatching:scheme = 1', 
+            'JetMatching:merge = on', 
+            'JetMatching:jetAlgorithm = 2', 
+            'JetMatching:etaJetMax = 5.', 
+            'JetMatching:coneRadius = 1.', 
+            'JetMatching:slowJetPower = 1', 
+            'JetMatching:qCut = 23.', 
+            'JetMatching:nQmatch = 5', 
+            'JetMatching:nJetMax = 2', 
+            'JetMatching:doShowerKt = off'),
+        pythia8CP5Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:ecmPow=0.03344', 
+            'PDF:pSet=20', 
+            'MultipartonInteractions:bProfile=2', 
+            'MultipartonInteractions:pT0Ref=1.41', 
+            'MultipartonInteractions:coreRadius=0.7634', 
+            'MultipartonInteractions:coreFraction=0.63', 
+            'ColourReconnection:range=5.176', 
+            'SigmaTotal:zeroAXB=off', 
+            'SpaceShower:alphaSorder=2', 
+            'SpaceShower:alphaSvalue=0.118', 
+            'SigmaProcess:alphaSvalue=0.118', 
+            'SigmaProcess:alphaSorder=2', 
+            'MultipartonInteractions:alphaSvalue=0.118', 
+            'MultipartonInteractions:alphaSorder=2', 
+            'TimeShower:alphaSorder=2', 
+            'TimeShower:alphaSvalue=0.118'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.datamixing_step = cms.Path(process.pdatamix)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.PREMIXRAWoutput_step = cms.EndPath(process.PREMIXRAWoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.datamixing_step,process.L1simulation_step,process.digi2raw_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.endjob_step,process.PREMIXRAWoutput_step])
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(8)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.GenProduction.customiseGenSimRawAodsim
+from Configuration.GenProduction.customiseGenSimRawAodsim import noLumiCheck,randomSeed 
+
+#call to customisation function noLumiCheck imported from Configuration.GenProduction.customiseGenSimRawAodsim
+process = noLumiCheck(process)
+
+#call to customisation function randomSeed imported from Configuration.GenProduction.customiseGenSimRawAodsim
+process = randomSeed(process)
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
+
+#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforMC(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
