7d1eda29f20a0efe7a7b5cb9ab66e6ddf83129f7
Pierluigi Bortignon <bortigno@gmail.com>
2018-07-30 17:05:23 +0200

    Adding options to make the script more flexible

--- analysis-tools/skim_tree.py
+++ analysis-tools/skim_tree.py
@@ -4,20 +4,28 @@ import os, pickle, sys, ROOT, logging
 ROOT.gROOT.SetBatch(True)
 from optparse import OptionParser
 # FIXME: this will not work unless it is in heppynmore python directory.
+sys.path.append('/afs/cern.ch/user/b/bortigno/workspace/heppynmore-dev/python/')
 from myutils import copytree
 
 argv = sys.argv
 
 #get files info from config
 parser = OptionParser()
+parser.add_option("-F", "--filein", dest="fileIN", default="",
+                      help="input file")
 parser.add_option("-I", "--pathin", dest="pathIN", default="",
                       help="input directory")
 parser.add_option("-O", "--pathout", dest="pathOUT", default="",
                               help="pathOUT")
 parser.add_option("-C", "--cut", dest="cut_string", default="",
                               help="cut string")
+parser.add_option("-P","--outputfile-prefix",dest="output_prefix",default="",
+                  help="String prefix for outoutfile")
 (opts, args) = parser.parse_args(argv)
 
-for f in os.listdir(opts.pathIN):
+if not (opts.fileIN == ""):
+  copytree(os.path.dirname(os.path.abspath(opts.fileIN)),opts.pathOUT,'',opts.output_prefix,os.path.basename(opts.fileIN),'',opts.cut_string)
+else:
+  for f in os.listdir(opts.pathIN):
     if not f.endswith('.root'): continue
-    copytree(opts.pathIN,opts.pathOUT,'','',os.path.splitext(f)[0],'',opts.cut_string)
+    copytree(opts.pathIN,opts.pathOUT,'',opts.output_prefix,os.path.splitext(f)[0],'',opts.cut_string)
