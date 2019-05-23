4b8b9501ee361d6fd0d8f327865cc309f5e08613
Pierluigi Bortignon <bortigno@gmail.com>
2019-05-23 15:23:27 +0200

    updated

--- analysis-tools/listSamplesInfoFromDAS.py
+++ analysis-tools/listSamplesInfoFromDAS.py
@@ -14,11 +14,29 @@ from python.Samples import *
 import gc
 import re
 
+
+def get_sample_list(sample_group):
+  sample_list = []
+  if sample_group == 'all':
+    # get all objects in sample
+    sample_list = [o for o in gc.get_objects() if isinstance(o, sample)]
+  elif sample_group == 'background':
+    # get only background
+    sample_list = Background
+  elif sample_group == 'signal':
+    # get only the signal
+    sample_list = Signal 
+  else:
+    print('No valid sample group')
+  return sample_list
+
 def main():
 #  args=sys.argv[1:]
 #  data=args[0]
-  sample_list = []
-  sample_list = [o for o in gc.get_objects() if isinstance(o, sample)]
+
+  sample_group = 'signal' # signal, background, data, all
+  sample_list = get_sample_list(sample_group)
+  sample_list.sort()
 
   url="https://cmsweb.cern.ch/dbs/prod/global/DBSReader"
   api=DbsApi(url=url)
@@ -30,13 +48,13 @@ def main():
  
     if outputDataSets:
       for ds in outputDataSets:
-       print('{0}'.format(ds['dataset']))
-       print('{0}'.format(ds['primary_ds_name']))
-       print('{0}'.format(ds['xtcrosssection']))
+       #print('{0}'.format(ds['dataset']))
+       #print('{0}'.format(ds['primary_ds_name']))
+       #print('{0}'.format(ds['xtcrosssection']))
        nevents = api.listBlockSummaries(dataset=ds['dataset'])
-       print(nevents[0]['num_event'])    
+       #print(nevents[0]['num_event'])    
        # this to create a table for the paper with dataset name and number of events 
-       print('verb@ {0} & {1}'.format(ds['primary_ds_name'],nevents[0]['num_event'])) 
+       print('verb@ {0} @ & {1:.2e} & XX \\\\ '.format(ds['primary_ds_name'],nevents[0]['num_event'])) 
   sys.exit(0);
 
 if __name__ == "__main__":
